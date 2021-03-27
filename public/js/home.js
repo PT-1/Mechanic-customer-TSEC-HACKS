9c8834214646d26e6fc08be6008c1b5eba6d55f4
ranjeet112-mum <ranjeet112pandey@gmail.com>
2021-03-27 21:07:46 +0530

    getting bored af!

--- public/js/home.js
+++ public/js/home.js
@@ -1,13 +1,16 @@
 
 window.onload = function checkCategory() {
-    var category = "@category";
-    // const category = document.getElementById('category').getAttribute('category_type');
+    // var category = "@category";
+    const category = document.querySelector('#passVar').innerText;
     // const category = $category.getAttribute('category_type');
-    console.log("category" + category);
-    if (category === "Mechanic") {
-        const customerForm = document.getElementById("#customerForm");
-        customerForm.style.display = 'none';
-        // customerForm.className += " cust";
+    console.log(category + " "+ "this is in the cliend js of home");
+   
+    // console.log("category" + category);
+    if (category == " Mechanic ") {
+        const customerForm = document.querySelector("#customerForm");
+    
+    console.log(customerForm);
+    customerForm.className += " cust";
 
     }
 }
