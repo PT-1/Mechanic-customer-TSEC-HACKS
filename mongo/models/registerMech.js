aea671d8effba2715fd29003f0f2c801663fb371
ranjeet112-mum <ranjeet112pandey@gmail.com>
2021-03-27 09:46:49 +0530

    hello world

--- mongo/models/registerMech.js
+++ mongo/models/registerMech.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const registeration = new mongoose.model('resgisterMech',{
+const registeration = new mongoose.model('resgister',{
     fullname : {
         type:String,
         required : true,
@@ -17,10 +17,8 @@ const registeration = new mongoose.model('resgisterMech',{
         type: String,
         required : true,
     },
-    username : {
-        type : String,
-        unique:true,
-        required:true,
+    category :{
+        
     },
     password: {
         type :String,
